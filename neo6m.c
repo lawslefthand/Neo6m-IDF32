c9d53b1f1a589cce09f2df2b0a37f3bab74ed804
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-08-16 15:34:55 +0530

    Update neo6m.c

--- neo6m.c
+++ neo6m.c
@@ -10,6 +10,11 @@
 #include "hal/uart_types.h"
 #include "portmacro.h"
 #include "esp_log.h"
+#include "string.h"
+
+
+
+static char buf[BUFFER]; 
 
 static const char *TAG = "GPS_MODULE";
 
@@ -26,44 +31,60 @@ void gps_start(void)
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(uart_num, 1, 3, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
     ESP_ERROR_CHECK(uart_driver_install(uart_num, BUFFER, 0, 0, NULL, 0));
+    
+
 }
 
 void raw_nmea(void)
-{
-    char buf[BUFFER];
+{ 
+    memset(buf, 0, BUFFER);
+	char buf[BUFFER];
     uart_read_bytes(UART_NUM_0, buf, BUFFER, portMAX_DELAY);
-    ESP_LOGI(TAG, "%s", buf);
+   ESP_LOGI(TAG, "%s", buf);
+}
+
+void parse_nmea(const char *buf, char *latitude, char *longitude) {
+    const char *start;
+    char lat[12], lon[12];
+
+  
+    start = strstr(buf, "$GPGGA");
+    if (start != NULL) {
+        sscanf(start, "$GPGGA,%*f,%11[^,],%*c,%11[^,],%*c", lat, lon);
+        sprintf(latitude, "%s", lat);
+        sprintf(longitude, "%s", lon);
+    }
 }
 
 void lat_long(void)
 {
-    char buf[BUFFER];
+    
     char lat[12];
     char lon[12];
-    uart_read_bytes(UART_NUM_0, buf, BUFFER, 20 / portTICK_PERIOD_MS);
-    sscanf(buf, "$GPGGA,%*f,%11[^,],%*c,%11[^,],%*c", lat, lon);
-    ESP_LOGI(TAG, "Latitude is:%s", lat);
-    ESP_LOGI(TAG, "Longitude is:%s", lon);
-}
+    void parse_nmea(const char *buf, char *lat, char *lon);
+    ESP_LOGI(TAG, "Latitude is:%s\n", lat);
+    ESP_LOGI(TAG, "Longitude is:%s\n", lon);
+  }
 
 void time(void)
 {
-    char buf[BUFFER];
+    
     char time[20];
-    uart_read_bytes(UART_NUM_0, buf, BUFFER, 20 / portTICK_PERIOD_MS);
-    sscanf(buf, "$GPGGA,%6[^,],", time); 
+    sscanf(buf, "$GPRMC,%10[^,]", time); 
     ESP_LOGI(TAG, "Time is %s", time); 
+    memset(time, 0, 20);
 }
 
 void speed_course(void)
 {
-    char buf[BUFFER];
+    
     float speedKmh;
     float course;
-    uart_read_bytes(UART_NUM_0, buf, BUFFER, 20 / portTICK_PERIOD_MS);
     sscanf(buf, "$GPVTG,%*f,%*c,%*f,%*c,%*f,%*c,%f", &speedKmh); 
     sscanf(buf, "$GPVTG,%f", &course); 
     sscanf(buf, "$GPRMC,%*f,%*c,%*f,%*c,%*f,%*c,%*f,%f", &course); 
     ESP_LOGI(TAG, "Speed is %f", speedKmh); 
     ESP_LOGI(TAG, "Course is %f", course); 
+   
+   
 }
