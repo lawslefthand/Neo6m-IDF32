2280d550de36d44b01630f68b62c473ad4403b55
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-08-16 15:35:21 +0530

    Update neo6m.h

--- neo6m.h
+++ neo6m.h
@@ -1,34 +1,30 @@
-/*
- * neo6m.h
- *
- *  Created on: 15-Aug-2024
- *      Author: danba
- */
 
-#ifndef MAIN_NEO6M_H_
-#define MAIN_NEO6M_H_
 
-#include <stdint.h>
+#ifndef SG90_H_
+#define SG90_H_
+
 #include <stdio.h>
-#include <stdbool.h>
 #include <sys/_intsup.h>
-#include <unistd.h>
-#include "driver/uart.h"
+#include "driver/ledc.h"
 #include "esp_err.h"
-#include "freertos/idf_additions.h"
-#include "hal/uart_types.h"
+#include "freertos/FreeRTOS.h"
+#include "hal/ledc_types.h"
 #include "portmacro.h"
 #include "esp_log.h"
 
-#define BUFFER 1024*2
+//PWM settings
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (819) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY          (50) // Frequency in Hertz. Set frequency at 4 kHz
 
- void gps_start(void);
- void raw_nmea(void);
- void lat_long(void);
- void speed_course(void);
- void time(void);
+void servo_init(void);
+void servo_swerve(void);
+void servo_pos(int pos);
+void parse_nmea(const char *buf, char *latitude, char *longitude);
 
 
-
-
-#endif /* MAIN_NEO6M_H_ */
+#endif 
